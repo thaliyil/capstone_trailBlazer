51c075cadfe2161c98cc512c6042287cf342f60f
Fathimath <faseela863gqqQq@@gmail.com>
2024-07-24 17:34:46 +0200

    handle error in vercel

--- components/ActivityDetails/index.js
+++ components/ActivityDetails/index.js
@@ -1,31 +1,29 @@
-
 import Image from "next/image";
 import { StyledCategory } from "../ActivitiesList/ActivityCardPreview";
 import { StyledCateogriesUl } from "../ActivitiesList/ActivityCardPreview";
 
-export default function ActivityDetails({activity}) {
-  const { id,title, categories, imageUrl, area, country, description } =
-    activity; 
+export default function ActivityDetails({ activity }) {
+  const { title, categories, imageUrl, area, country, description } = activity;
 
   return (
-  
-  <>
-  <h2>{title}</h2>
-    <Image
-    src={imageUrl}
-    layout="responsive"
-    width={400}
-    height={360}
-    alt={title}
-  />
-   <h3>{area}, {country}</h3>
-   <p>{description}</p>
-  <StyledCateogriesUl>
-    {categories.map((category) => (
-      <StyledCategory key={id}>{category}</StyledCategory>
-    ))}
-  </StyledCateogriesUl>
- 
-  </>
+    <>
+      <h2>{title}</h2>
+      <Image
+        src={imageUrl}
+        layout="responsive"
+        width={400}
+        height={360}
+        alt={title}
+      />
+      <h3>
+        {area}, {country}
+      </h3>
+      <p>{description}</p>
+      <StyledCateogriesUl>
+        {categories.map((category) => (
+          <StyledCategory key={activity.id}>{category}</StyledCategory>
+        ))}
+      </StyledCateogriesUl>
+    </>
   );
 }
