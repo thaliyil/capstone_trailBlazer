9607ec9793213524db4eedf4f5927734a1d454f0
Rebecca Pavelt <rebeccapavelt@gmx.de>
2024-08-21 21:50:09 +0200

    add logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,16 +1,34 @@
 import styled from "styled-components";
+import Image from "next/image";
+import logo from "../../assets/logo.png";
 
 export default function Header() {
-  return <StyledHeader>TrailBlazer</StyledHeader>;
+  return (
+    <>
+      <StyledHeader>TrailBlazer</StyledHeader>
+      <StyledLogo src={logo} alt="TrailBlazer Logo" />
+    </>
+  );
 }
 
 const StyledHeader = styled.h1`
   background-color: var(--dijon);
-  padding: 10px 0;
+  padding: 20px 0;
   margin: 0;
   border: 1px solid grey;
   position: fixed;
   width: 100vw;
   z-index: 3;
   box-shadow: var(--box-shadow-layout);
+`;
+
+const StyledLogo = styled(Image)`
+  width: 50px;
+  height: 60px;
+  position: absolute;
+  z-index: 50;
+  position: fixed;
+  left: 40px;
+  border-radius: 5px;
+  top: 10px;
 `;
